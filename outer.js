c773ee5cbcf8e153d2fca231da2613c4625de976
Lucyna Powałowska <lucyna.powalowska@gmail.com>
2017-06-25 19:13:51 +0200

    FZ5WK-29/ add smooth scrolling

--- outer.js
+++ outer.js
@@ -1,9 +1,25 @@
+
+/*Shrinked navbar*/
+
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 70) {
         $(".navbar--height").addClass("navbar--height--shrink");
     } else {
         $(".navbar--height").removeClass("navbar--height--shrink");
     }
+});
+
+
+/*document.getElementById('.navbar')*/
+
+
+/*Smooth scrolling:*/
+
+$('.nav a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
 });
 
 
