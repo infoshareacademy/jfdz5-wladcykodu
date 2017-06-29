fc80fa099f43cd33ac6609f8856658256bd0638e
rafals7 <rafal.sobiepan@gmail.com>
2017-06-29 23:06:17 +0200

    feature/FZ5WK-28: Added translation toggle flags

--- translate.js
+++ translate.js
@@ -7,4 +7,13 @@ function translatePage() {
         this.innerText = this.getAttribute('data-lang');
         this.setAttribute('data-lang', oldValue);
     });
+    $('.language-toggle').each(function() {
+        var oldSrc = this.getAttribute('src');
+        var oldAlt = this.getAttribute('alt');
+        this.setAttribute('src', this.getAttribute('data-flag'));
+        this.setAttribute('alt', this.getAttribute('data-alt'));
+        this.setAttribute('title', this.getAttribute('data-alt'));
+        this.setAttribute('data-flag', oldSrc);
+        this.setAttribute('data-alt', oldAlt);
+    });
 };
