9368f8ccee5cc2d15696c4d979879d1c21866ce9
ajemielity <ajemielity@o2.pl>
2017-07-01 11:54:10 +0200

    correct cookies-info

--- cookies.js
+++ cookies.js
@@ -2,24 +2,9 @@
  ** Show cookies-info when user visits our site more than 30 days ago **
  **********************************************************************/
 
-/**
-localStorage.hasOwnProperty("DATE")
-    KIEDY
-    false
-
-        I WYSWIETLIJ DIVA Z KOMUNIKATEM
-    true
-        localStorage.getItem("DATE")
-            JEŚLLI
-            "DATE" PONIZEJ 30 DNI TO
-                NIC
-            JESLI "DATE" POWYŻEJ 30 DNI TO
-                localStorage.setItem("DATE", "DATA WEJSCIA NA STR"); /CZYLI PODMIANA DATY/
-                I WYSWIETLIJ DIVA Z KOMUNIKATEM
-**/
-
 
 /*
+notatki:
  getTime()	- zwraca aktualny czas jako liczbę reprezentującą liczbę milisekund która upłynęła od godziny 00:00 1 stycznia 1970 roku  --> (new Date).getTime()
 30 day = 720 h = 43 200 min = 2 592 000 s = 2 592 000 000 ms
 */
