858b7387dc7074efd3c7296b71ada777395ffbb9
rafals7 <rafal.sobiepan@gmail.com>
2017-07-05 21:23:16 +0200

    feature/FZ5WK-28-rs: added new translations

--- gototop.js
+++ gototop.js
@@ -1,20 +1,20 @@
-/*****************************************************************************
- * Hiding and showing "go back to top" button when scrolling.                *
- * Smooth scrolling to top of document when "go back to top" button clicked. *
- *****************************************************************************/
+/************************************************************************
+ * Hiding and showing "go to top" button when scrolling.                *
+ * Smooth scrolling to top of document when "go to top" button clicked. *
+ ************************************************************************/
 
 $(document).ready(function(){
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 150) {
-            $('#go-back-to-top').fadeIn();
+            $('#go-to-top').fadeIn();
         } else {
-            $('#go-back-to-top').fadeOut();
+            $('#go-to-top').fadeOut();
         }
     });
 
     // scroll body to 0px on click
-    $('#go-back-to-top').click(function () {
+    $('#go-to-top').click(function () {
         $('html, body').animate({
             scrollTop: 0
         }, 500);
