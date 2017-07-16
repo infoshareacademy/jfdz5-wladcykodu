e3fb2df583eeced5db2d8a9292b32f318edcda8e
rafals7 <rafal.sobiepan@gmail.com>
2017-07-17 00:11:48 +0200

    feature/FZ5WK-38:Correction - now obstacles do not stop when they get to the bottom of game board.

--- obstacles.js
+++ obstacles.js
@@ -1,26 +1,22 @@
 /**
  * Created by rafal on 15.07.17.
  */
-function times(n, callback) {
-    for (var i = 0; i < n; i += 1) {
-        callback(i);
-    }
-}
-
 function setObstacles() {
     var $boardFileds = $('.board-field');
     var rowSize = 31;
-    var rowCount = 24;
+    var rowCount = 23;
     var counter = 0;
 
     setInterval(function () {
-        for (var i = rowCount - 2; i >=0; i -= 1) {
+        for (var i = rowCount - 1; i >=0; i -= 1) {
             for (var j = 0; j < rowSize ; j += 1) {
-                oldCellIndex = i * rowSize + j;
-                newCellIndex = oldCellIndex + rowSize;
-                if ($boardFileds[oldCellIndex].classList.contains('board-field--obstacle')) {
-                    $boardFileds[newCellIndex].classList.add('board-field--obstacle');
-                    $boardFileds[oldCellIndex].classList.remove('board-field--obstacle');
+                currentCellIndex = i * rowSize + j;
+                nextCellIndex = currentCellIndex + rowSize;
+                if ($boardFileds[currentCellIndex].classList.contains('board-field--obstacle')) {
+                    if (i !== rowCount - 1) {
+                        $boardFileds[nextCellIndex].classList.add('board-field--obstacle');
+                    }
+                    $boardFileds[currentCellIndex].classList.remove('board-field--obstacle');
                 }
             }
         }
