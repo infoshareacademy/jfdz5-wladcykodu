3c8bc921d18126754f113b77c38dc88e1f780a0c
lucypow <lucyna.powalowska@gmail.com>
2017-07-22 00:00:49 +0200

    feature/FZ5WK-39/ added quiz-board after click and started function quizForMoreLifes()

--- goodElements.js
+++ goodElements.js
@@ -62,7 +62,7 @@ function createBonus(gameclass) {
         var $quiz = $('.bonus-life');
         $quiz.on('click', function () {
             console.log('clicked');
-            $('.game-in-progress').append('<div class="quiz-board higher-z-index"></div>');
+            $('.game-in-progress').append('<div class="quiz-board higher-z-index"><div class="quiz"><div><div class="question">Result of <span class="quiz-text" id="multiplicand">1</span> * <span class="quiz-text" id="multiplier">1</span>  is: </div><div class="answers"><div class="option"><div class="answer-box" id="answer1">1</div></div><div class="option"><div class="answer-box" id="answer2">1</div></div><div class="option"><div class="answer-box" id="answer3">1</div></div></div></div>');
             clearInterval(bonus);
         });
     }
@@ -101,4 +101,15 @@ function createBonus(gameclass) {
         $(this).css('visibility', 'hidden');
         document.dispatchEvent(new CustomEvent('score', {detail: {action: "add", value: 10}}));
     });
+}
+
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function quizForMoreLifes() {
+    var multiplicand = randomNum(1, 10);
+    var multiplier = randomNum(1, 10);
+    var correctResult = multiplicand * multiplier;
+
 }
--- timeout.js
+++ timeout.js
@@ -2,5 +2,5 @@ $('.start-game-button').on('click', function () {
     setTimeout(function () {
         clearInterval(bonus); // add other intervals to clear...
         console.log('clear interval bonus');
-    }, 60000); // it will be e.g 600000 = 10 min, now it is 1 minute
+    }, 600000); // 10 minutes
 });
